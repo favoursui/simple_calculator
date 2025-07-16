b26ddda7a13158ed4eec3732dddef4025703dcc6
favoursui <samuelfavour858@gmail.com>
2025-07-16 13:06:13 +0100

    first commit

---
+++ simple_calculator.py
@@ -0,0 +1,75 @@
+# A simple calculator
+
+print ('''1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+''')
+
+print ("enter two numbers to add")
+
+# accepts input from the user and stores it
+first_number = int(input("enter first number: "))
+second_number = int(input("enter second number: "))
+
+# sum up the inputs collected and convert to float
+sum = float(first_number) + float(second_number)
+print ("{} + {} = {:,.2f}" .format(first_number, second_number, sum))
+
+
+print ("enter two numbers to subtract")
+   
+# accepts input from the user and stores it
+first_number = int(input("enter first number: "))
+second_number = int(input("enter second number: "))
+ 
+# make a subtraction fom the inputs collected and convert to float
+sub = float(first_number) - float(second_number)
+print ("{} - {} = {:,.2f}" .format(first_number, second_number, sub))
+
+
+print ("enter two numbers to multiply")
+  
+# accepts input from the user and stores it
+first_number = int(input("enter first number: "))
+second_number = int(input("enter second number: "))
+  
+# make a multiplication with the inputs collected and convert to float
+mul = float(first_number) * float(second_number)
+print ("{} * {} = {:,.2f}" .format(first_number, second_number, mul))
+
+
+print ("enter two numbers to divide")
+  
+# accepts input from the user and stores it
+first_number = int(input("enter first number: "))
+second_number = int(input("enter second number: "))
+  
+# make a division with the inputs collected and convert to float
+div = float(first_number) / float(second_number)
+print ("{} / {} = {:,.2f}" .format(first_number, second_number, div))
+
+
+print ("enter a number to exponent")
+ 
+# accepts input from the user and stores it
+first_number = int(input("enter first number: "))
+second_number = int(input("enter second number: "))
+ 
+# make an exponential
+expo = float(first_number) ** float(second_number)
+print ("{} ** {} = {:,.2f}" .format(first_number, second_number, expo))
+ 
+
+
+  
+ 
+
+
+
+
+
+
+
+
+
